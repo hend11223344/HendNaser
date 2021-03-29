877a4f7766226784c9cf3777ae8983282829d6de
hend11223344 <78754796+hend11223344@users.noreply.github.com>
2021-03-29 12:13:53 +0300

    Add files via upload

---
+++ FORTH2_HOMEWORK.java
@@ -0,0 +1,90 @@
+public class FORTH2_HOMEWORK
+{
+    static class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d;
+            next = null;}
+    }
+    static class MergeLists
+    {
+        Node head;
+        public void addToTheLast(Node node)
+        {
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                    temp = temp.next;
+                temp.next = node;
+            }
+        }
+        void printList()
+        {
+            Node temp = head;
+            while (temp != null)
+            {
+                System.out.print(temp.data + " ");
+                temp = temp.next;
+            }
+            System.out.println();
+        }
+        public static void main(String args[])
+        {
+
+            MergeLists llist1 = new MergeLists();
+            MergeLists llist2 = new MergeLists();
+
+            llist1.addToTheLast(new Node(3));
+            llist1.addToTheLast(new Node(6));
+            llist1.addToTheLast(new Node(9));
+
+            llist2.addToTheLast(new Node(2));
+            llist2.addToTheLast(new Node(4));
+            llist2.addToTheLast(new Node(8));
+
+            llist1.head = new Gfg().sortedMerge(llist1.head,
+                   llist2.head);
+            llist1.printList();
+        }
+    }
+    static class Gfg
+    {
+        Node sortedMerge(Node headA, Node headB)
+        {
+            Node dummyNode = new Node(0);
+
+            Node tail = dummyNode;
+            while(true)
+            {
+                if(headA == null)
+                {
+                    tail.next = headB;
+                    break;
+                }
+                if(headB == null)
+                {
+                    tail.next = headA;
+                    break;
+                }
+                if(headA.data <= headB.data)
+                {
+                    tail.next = headA;
+                    headA = headA.next;
+                }
+                else
+                {
+                    tail.next = headB;
+                    headB = headB.next;
+                }
+                tail = tail.next;
+            }
+            return dummyNode.next;
+        }
+    }
+}
