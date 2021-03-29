18fb3cc0876a197fc9fa20c45702b8c9dc10d8ac
hend11223344 <78754796+hend11223344@users.noreply.github.com>
2021-03-29 11:12:07 +0300

    Add files via upload

---
+++ FORTH1_HOMEWORK.java
@@ -0,0 +1,55 @@
+public class FORTH1_HOMEWORK
+{static class Node
+    {
+        int data;
+        Node next;
+        Node(int d)
+        {
+            this.data = d;
+            this.next = null;
+        }
+    }
+    static class LinkedList
+    {
+        Node start;
+        LinkedList()
+        {
+            start = null;
+        }
+        public void push(int data)
+        {
+            if(this.start == null)
+            {
+                Node temp = new Node(data);
+                this.start = temp;
+            }
+            else
+            {
+                Node temp = new Node(data);
+                temp.next = this.start;
+                this.start = temp;
+            }
+        }
+        public int findSecondLastNode(Node ptr)
+        {
+            Node temp = ptr;
+            if(temp == null || temp.next == null)
+                return -1;
+            while(temp.next.next != null)
+            {
+                temp = temp.next;
+            }
+            return temp.data;
+        }
+         public static void main(String[] args)
+        {
+            LinkedList ll = new LinkedList();
+            ll.push(12);
+            ll.push(1);
+            ll.push(11);
+            ll.push(23);
+            ll.push(8);
+            System.out.println(ll.findSecondLastNode(ll.start));
+        }
+    }
+}
